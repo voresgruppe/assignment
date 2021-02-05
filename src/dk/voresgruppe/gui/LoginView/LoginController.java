caf797e5d987733299f58cb98d2894313fc7ded4
Emil Hallum Hansen <emil875b@easv365.dk>
2021-02-05 11:41:16 +0100

    rescources

--- src/dk/voresgruppe/gui/LoginView/LoginController.java
+++ src/dk/voresgruppe/gui/LoginView/LoginController.java
@@ -2,14 +2,23 @@ package dk.voresgruppe.gui.LoginView;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class LoginController {
     public TextField UserID;
-    public TextField PassID;
+    public PasswordField PassID;
+    
     public CheckBox cboxRememberMe;
     public ImageView imgCompanyLogo;
+    public ImageView imgPwIcon;
+
+    public void initialize(){
+        //imgPwIcon = new ImageView(new Image(getClass().getResourceAsStream("pw_eye_visibility_off.png"))); ???
+    }
+
 
     public void btnLogin(ActionEvent actionEvent) {
     }
