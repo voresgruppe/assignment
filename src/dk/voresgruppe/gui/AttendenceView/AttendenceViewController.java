f27616323e8b9495cf02e862def7a3d2dfaeaf5a
ceci6182 <ceci6182@easv365.com>
2021-02-05 11:26:46 +0100

    changes

---
+++ src/dk/voresgruppe/gui/AttendenceView/AttendenceViewController.java
@@ -0,0 +1,12 @@
+package gui.AttendenceView;
+
+import javafx.event.ActionEvent;
+
+public class AttendenceViewController {
+
+    public void handleRegisterAttendence(ActionEvent actionEvent) {
+    }
+
+    public void handleLogOut(ActionEvent actionEvent) {
+    }
+}
