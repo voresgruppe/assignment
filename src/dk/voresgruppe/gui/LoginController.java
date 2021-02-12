d9c5524280bf99dca564b0a77333f8bf7172d6bf
Emil Hallum Hansen <emil875b@easv365.dk>
2021-02-12 10:37:37 +0100

    showhidepass

--- src/dk/voresgruppe/gui/LoginController.java
+++ src/dk/voresgruppe/gui/LoginController.java
@@ -5,24 +5,38 @@ import dk.voresgruppe.be.Student;
 import dk.voresgruppe.be.User;
 import dk.voresgruppe.bll.StudentManager;
 import dk.voresgruppe.gui.AttendenceView.AttendanceViewController;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+    public BorderPane borderPaneTxtField;
+    public BorderPane borderPanePassField;
+    public StackPane stackPane;
+    public TextField txtPassShown;
     StudentManager sMan = new StudentManager();
     public TextField UserID;
     public PasswordField PassID;
@@ -30,6 +44,8 @@ public class LoginController implements Initializable {
     public CheckBox cboxRememberMe;
     public ImageView imgCompanyLogo;
     public ImageView imgPwIcon;
+
+    private boolean hidePass;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -41,7 +57,14 @@ public class LoginController implements Initializable {
         }
         UserID.setText("jane1988");
         PassID.setText("jane1988");
+
+        hidePass = true;
+        addListener();
+
+
     }
+
+
 
     public void btnLogin(ActionEvent actionEvent) {
         if(!UserID.getText().isEmpty() && !PassID.getText().isEmpty()) {
@@ -71,5 +94,45 @@ public class LoginController implements Initializable {
         System.exit(1);
     }
 
+
+    public void showPass(MouseEvent mouseEvent) {
+        ObservableList<Node> workingCollection = FXCollections.observableArrayList(stackPane.getChildren());
+
+        if (hidePass){
+            File file = new File("image/pw_eye_visibility_off.png");
+            Image image = new Image(String.valueOf(file));
+            imgPwIcon.setImage(image);
+            hidePass = false;
+            Collections.swap(workingCollection, 0, 1);
+            stackPane.getChildren().setAll(workingCollection);
+        }else {
+            File file = new File("image/pw_eye_visibility.png");
+            Image image = new Image(String.valueOf(file));
+            imgPwIcon.setImage(image);
+            hidePass = true;
+            Collections.swap(workingCollection, 1, 0);
+            stackPane.getChildren().setAll(workingCollection);
+        }
+
+    }
+
+    public void addListener(){
+        txtPassShown.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                PassID.setText(newValue);
+            }
+        });
+
+        PassID.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+                txtPassShown.setText(newValue);
+            }
+        });
+        if(!PassID.getText().isEmpty()){
+            txtPassShown.setText(PassID.getText());
+        }
+    }
 
 }
